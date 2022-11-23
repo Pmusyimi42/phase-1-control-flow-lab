30957281ce333741ee9e56c1ecae090e09b1a79b
patrick kisyula <pmusyimi42@gmail.com>
2022-11-23 17:10:10 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if(distance <= 400){
+    let message = 'This one is on me!'
+    return message;
+  }
+  else if (distance > 2000 && distance<=2500){
+    let message = 'I will gladly take your thirty bucks.'
+    return message;
+  }
+  else if (distance > 2500){
+    let message = 'No can do.'
+    return message;
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(cityName){
   // Write your code here!
+  const message = cityName === 'NYC' ? "Ok, sounds good." : 'No go.'
+  return message;
 }
-
-function switchOnCharmFromTip(){
+// ternaryCheckCity('NYC');
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let message;
+  // tip = "generous"
+  switch (tip) {
+    case "generous": 
+      message = 'Thank you so much.'
+      // console.log(message);
+      break;
+    case 'not as generous':
+      message = "Thank you."
+      break;
+    default:
+      message = 'Bye.'
+  }
+  return message;
 }
+// switchOnCharmFromTip("generous")
